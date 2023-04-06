889a3b34f20200be7ff40e83a07e0166528fc51f
jheart-vic <adebowalevictorjheart@gmail.com>
2023-04-06 07:15:20 +0100

    Design Login Page Properly

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import React from 'react'
 import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import './App.css'
 import Login from './component/Login'
-// import Navbar from './component/Navbar'
 
 const  App = () => {
   return (
     <div className="App">
 <Router>
-    {/* <Navbar/> */}
 <Routes>
  <Route path='/login' element={<Login />}/>
 </Routes>
