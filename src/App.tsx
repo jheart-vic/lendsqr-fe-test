d84e346a8c362ed9dff5e07d1364e6449e6c1f3a
jheart-vic <adebowalevictorjheart@gmail.com>
2023-04-06 07:14:37 +0100

    Deign Login Properly

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,18 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom'
 import './App.css'
 import Login from './component/Login'
+// import Navbar from './component/Navbar'
 
 const  App = () => {
   return (
     <div className="App">
-     <Login />
+<Router>
+    {/* <Navbar/> */}
+<Routes>
+ <Route path='/login' element={<Login />}/>
+</Routes>
+</Router>
     </div>
   )
 }
